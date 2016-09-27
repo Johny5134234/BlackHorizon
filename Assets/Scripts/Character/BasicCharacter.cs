3b5291961dfa3b0a4411d96aff3887c71f9779c9
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-09-27 14:16:32 +1300

    Adding Single Jump Functionality

--- Assets/Scripts/Character/BasicCharacter.cs
+++ Assets/Scripts/Character/BasicCharacter.cs
@@ -1,28 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
-public class BasicCharecter : MonoBehaviour {
+public class BasicCharacter : MonoBehaviour {
 	public float moveSpeed;
 	public float jumpHeight;
 	public Transform groundCheck;
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
-	private bool grounded;
+
+	public float distToGround;
+	Rigidbody2D characterRigidBody;
+
 	// Use this for initialisation
 	void Start () {
+		characterRigidBody = GetComponent<Rigidbody2D>();
+		distToGround = collider.bounds.extents.y;
 	}
 	void fixedUpdate() {
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+
 	}
+
+	void isGrounded() {
+		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space) && grounded == true) {
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
 		}
 		if (Input.GetKey (KeyCode.D)) {
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 		if (Input.GetKey (KeyCode.A)) {
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 	}
 
