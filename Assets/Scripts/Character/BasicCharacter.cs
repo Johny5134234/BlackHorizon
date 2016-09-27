eebba3899c9bfef5e548cd837dd896c69ab9be7c
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-09-28 08:55:19 +1300

    Removing extraneous variables

--- Assets/Scripts/Character/BasicCharacter.cs
+++ Assets/Scripts/Character/BasicCharacter.cs
@@ -3,9 +3,6 @@ using System.Collections;
 public class BasicCharacter : MonoBehaviour {
 	public float moveSpeed;
 	public float jumpHeight;
-	public Transform groundCheck;
-	public float groundCheckRadius;
-	public LayerMask groundLayer;
 
 	float jumpCount = 0;
 	public float maxJumps = 2;
@@ -27,7 +24,7 @@ public class BasicCharacter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Rigidbody2D characterRigidBody = GetComponent<Rigidbody2D>();
-		if (Input.GetKeyDown (KeyCode.Space) && jumpCount <= maxJumps) {
+		if (Input.GetKeyDown (KeyCode.Space) && jumpCount < maxJumps) {
 			characterRigidBody.velocity = new Vector2 (characterRigidBody.velocity.x, jumpHeight);
 			jumpCount += 1;
 		}
@@ -38,4 +35,3 @@ public class BasicCharacter : MonoBehaviour {
 			characterRigidBody.velocity = new Vector2 (-moveSpeed, characterRigidBody.velocity.y);
 		}
 	}
-}
