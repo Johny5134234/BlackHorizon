e8effe3da5a2c7cf86e84093790fc5213d9d5aee
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-09-28 11:41:39 +1300

    Added ladder trigger for climbing ladders

--- Assets/LadderTrigger.cs
+++ Assets/LadderTrigger.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class LadderTrigger : MonoBehaviour {
 
+	public float ladderMoveSpeed = 0.15;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	void OnTriggerEnter2D(Collider2D other) {
+		other.AddForce(new Vector2(other.velocity.x, ladderMoveSpeed));
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
