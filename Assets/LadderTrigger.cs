271ae61cd6bc62f52ce22f90e88f5bebc8b2bef2
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-09-28 13:46:40 +1300

    Fixed function call for Collider2D

--- Assets/LadderTrigger.cs
+++ Assets/LadderTrigger.cs
@@ -11,7 +11,7 @@ public class LadderTrigger : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		other.AddForce(new Vector2(other.velocity.x, ladderMoveSpeed));
+		other.attachedRigidBody.AddForce(new Vector2(other.velocity.x, ladderMoveSpeed));
 	}
 
 	// Update is called once per frame
