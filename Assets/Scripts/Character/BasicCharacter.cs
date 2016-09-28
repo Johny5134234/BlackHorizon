4e3ed373c7748cfeddb4198fbebf66c81a35614a
Techno-Brony <zhiyuanqi@hotmail.co.nz>
2016-09-28 15:32:28 +1300

    Fixed incorrect method signature of FixedUpdate

--- Assets/Scripts/Character/BasicCharacter.cs
+++ Assets/Scripts/Character/BasicCharacter.cs
@@ -18,7 +18,7 @@ public class BasicCharacter : MonoBehaviour {
 	void Start () {
 	}
 
-	void fixedUpdate() {
+	void FixedUpdate() {
                 handleMovement();
                 handleShooting();
 	}
