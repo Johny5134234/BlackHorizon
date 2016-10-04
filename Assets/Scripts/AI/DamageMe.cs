237e340109ab8373787382f8568718d1f325187b
John Hayman <haymanj@me.com>
2016-10-04 16:32:47 +1300

    Added Tag PLAYER
     -Added the tag PLAYER
     -Expanded map

---
+++ Assets/Scripts/AI/DamageMe.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
